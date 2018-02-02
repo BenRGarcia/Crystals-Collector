fd433ca4e7c32883d084235b97ae094397e8a768
Ben Garcia <ben@seebenprogram.com>
2018-02-01 21:53:15 -0500

    Create 'Game' class, instantiate game object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,8 @@
-// Variables needed
-/*
- * win counter
- * loss counter
- * random number
- * player score
- * crystal 1 click point-value
- * crystal 2 click point-value
- * crystal 3 click point-value
- * crystal 4 click point-value
- * 
- */
-//
-
-// Functions needed
-/*
- * increase win count
- * increase loss count
- * random point value generator (1-12 for crystals, 19-120 for random number)
- * update DOM (js-random-number ,js-score ,js-win-count js-loss-count )
- * listen for click events (js-button-1, js-button-2, js-button-3, js-button-4)
- * add value of crystal clicked to player's score
- * test if user won
- * test if user lost
- * navigate logic decisions of game play
- */
-// 
-
-// Event listeners
-/*
- * listen for click events (js-button-1, js-button-2, js-button-3, js-button-4)
- */
-//
-
-// OOP thoughts
-/*
- * 4 'crystals' that have the same properties, but different values
- * Sounds like inheritance
- */
-//
-
-// MVC thoughts
-/*
- * View:
- *     - 1 object to handle view (DOM.render(component))
- * 
- * Model:
- *     - 1 parent class of 'crystal', 4 children objects: crystal1, crystal2...
- *       where each crystal object manages its own 'points' property value
- *       while using method from parent class to generate number
- *     - 1 object that manages game properties of random #, score, wins, losses,
- *       and required game play logic
- * 
- * Controller:
- *     - 1 object that evaluates game play logic
- * 
- */
-//
-
 //===========
 // Model
 //===========
 class Crystal {
+
   constructor() {
     this._pointValue;
     newPointValue();
@@ -71,24 +13,20 @@ class Crystal {
   }
 
   newPointValue() {
-    // Between 1-12
-    this._pointValue = Math.floor((Math.random() * 12) + 1);
+    this._pointValue = Math.floor((Math.random() * 12) + 1); // Between 1-12
   }
 }
 
-// instantiate crystal objects
-const crystal1 = new Crystal();
-const crystal2 = new Crystal();
-const crystal3 = new Crystal();
-const crystal4 = new Crystal();
+// Properties of the game
+class Game {
 
-
-// properties of the game
-const gameProps = {
-  _playerScore: 0,
-  winCount: 0,
-  lossCount: 0,
-  randomNumber: 0,
+  constructor() {
+    this._playerScore = 0;
+    this.winCount = 0;
+    this.lossCount = 0;
+    this.randomNumber;
+    generateRandomNumber();
+  }
 
   set playerScore(points) {
     if (typeof points === 'number') {
@@ -129,7 +67,17 @@ const gameProps = {
     this._playerScore = 0;
     this.generateRandomNumber();
   }
-};
+}
+
+
+// Instantiate crystal objects
+const crystal1 = new Crystal();
+const crystal2 = new Crystal();
+const crystal3 = new Crystal();
+const crystal4 = new Crystal();
+
+// Instantiate game object
+const crystalGameProps = new Game();
 
 
 //===========
@@ -142,19 +90,19 @@ const DOM = {
     switch (component) {
 
       case 'score':
-        $("#js-score").text(gameProps.playerScore);
+        $("#js-score").text(crystalGameProps.playerScore);
         break;
 
       case 'winCount':
-        $("#js-win-count").text(gameProps.winCount);
+        $("#js-win-count").text(crystalGameProps.winCount);
         break;
 
       case 'lossCount':
-        $("#js-loss-count").text(gameProps.lossCount);
+        $("#js-loss-count").text(crystalGameProps.lossCount);
         break;
 
       case 'randomNumber':
-        $("#js-random-number").text(gameProps.randomNumber);
+        $("#js-random-number").text(crystalGameProps.randomNumber);
         break;
 
       default:
@@ -163,9 +111,6 @@ const DOM = {
     }
   }
 };
-
-
-
 
 //===========
 // Controller
